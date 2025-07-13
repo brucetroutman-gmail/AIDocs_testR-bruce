5f30acf27c7c7ed6b1a862bfa9e3e9dceb02ed68
brucetroutman <bruce@imade3d.com>
2025-07-13 15:09:01 -0400

    Updated for AIDocs_testR

--- run-tests-all-models-gen-kno.sh
+++ run-tests-all-models-gen-kno.sh
@@ -6,7 +6,7 @@ function testit(){
 }
 
     testit "qwen2:1.5b"             # 0.934 GB
-    testit "gemma3:1b-it-q4_K_M"    # 0.815 GB
-    testit "granite3.3:2b"          # 1.5 GB
-    testit "llama3.2:3b"            # 2.0 GB
-    testit "phi4-mini:3.8b-q4_K_M"  # 2.5 GB
+#    testit "gemma3:1b-it-q4_K_M"    # 0.815 GB
+#    testit "granite3.3:2b"          # 1.5 GB
+#    testit "llama3.2:3b"            # 2.0 GB
+#    testit "phi4-mini:3.8b-q4_K_M"  # 2.5 GB
